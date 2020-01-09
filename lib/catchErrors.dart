5045895791ce179e6bf011cf2ea2d7b4096f4180
TheKingDave <daivd@langheiter.com>
2020-01-09 19:49:07 +0100

    No internal error to outside, print error + stacktrace

--- lib/catchErrors.dart
+++ lib/catchErrors.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:plain_github_keys/formatter.dart';
+import 'package:plain_github_keys/shelf_exception/exception.dart';
 import 'package:shelf/shelf.dart' as shelf;
 
 shelf.Middleware catchErrors() {
@@ -12,8 +13,11 @@ shelf.Middleware catchErrors() {
         }
         return response;
       }).catchError((error, stackTrace) {
-        final result =
-            formatter.formatResponse(request, {'message': error.toString()});
+        print('Error caught: ${error.toString()}');
+        print(stackTrace);
+
+        final e = HttpException();
+        final result = formatter.formatResponse(request, e.toMap());
         return shelf.Response(HttpStatus.internalServerError,
             body: result.body,
             headers: {HttpHeaders.contentTypeHeader: result.contentType});
