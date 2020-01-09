d267c23c2d3d24e5235c5874945cc1484a4ac2ca
TheKingDave <daivd@langheiter.com>
2020-01-09 13:28:17 +0100

    Added map text formatter

--- lib/formatter.dart
+++ lib/formatter.dart
@@ -5,6 +5,17 @@ ResponseFormatter formatter = ResponseFormatter()
     if (data is Iterable) {
       return data.join('\n');
     }
+    if (data is Map) {
+      if (data.containsKey('message')) {
+        return data['message'];
+      } else {
+        var ret = '';
+        for (String key in data.keys) {
+          ret += '$key: ${data[key]}\n';
+        }
+        return ret;
+      }
+    }
     return data.toString();
   })..registerFormatter('xml', 'text/xml', (dynamic data) {
     return xmlFormatter(data);
