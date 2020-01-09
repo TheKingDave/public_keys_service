05cc9fcf223d341371919643e56b487c70e7811e
TheKingDave <daivd@langheiter.com>
2020-01-09 13:28:36 +0100

    Added github username check

--- lib/services/GithubKeyService.dart
+++ lib/services/GithubKeyService.dart
@@ -1,11 +1,22 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:plain_github_keys/services/KeyService.dart';
+import 'package:plain_github_keys/shelf_exception/exception.dart';
+
+final userNameRegex = RegExp('^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}\$');
 
 class GithubKeyService implements KeyService {
   @override
   Future<List<String>> getKeys(String user) async {
+    user = user.toLowerCase();
+
+    if (!userNameRegex.hasMatch(user.toLowerCase())) {
+      throw HttpException(
+          HttpStatus.badRequest, 'Wrong username format for service');
+    }
+
     final uri = Uri.https('api.github.com', '/users/$user/keys');
     final resp = await http.get(uri);
 
