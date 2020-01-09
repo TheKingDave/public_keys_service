0fc70a63e7819f57bbb55aad94c5c6034936812c
TheKingDave <daivd@langheiter.com>
2020-01-09 19:49:40 +0100

    Use the right httpexception class

--- lib/shelf_exception/expection_response.dart
+++ lib/shelf_exception/expection_response.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:shelf/shelf.dart' as shelf;
 
 import '../formatter.dart';
+import 'exception.dart';
 
 ///A middleware that allows handlers to simply throw [HttpExceptions]
 ///instead of having to create and return a non successful [Response].
